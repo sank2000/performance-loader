066ae90badeaaec2815bf2cac789083505443358
sank2000 <santhoshvelr@yahoo.com>
2021-07-19 20:32:28 +0530

    reddis configured ✔

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cluster = require('cluster');
 const net = require('net');
@@ -73,7 +75,13 @@ if (cluster.isMaster) {
   // server is assumed to be on localhost:6379. You don't have to
   // specify them explicitly unless you want to change them.
   // redis-cli monitor
-  io.adapter(io_redis({ host: 'localhost', port: 6379 }));
+  io.adapter(
+    io_redis({
+      host: process.env.REDIS_ENDPOINT,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
+    })
+  );
 
   // Here you might use Socket.IO middleware for authorization etc.
   // on connection, send the socket over to our module with socket stuff
