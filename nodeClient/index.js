fc446951abf3f65b84a3d49c5eb90493d2197f09
sank2000 <santhoshvelr@yahoo.com>
2021-07-19 20:51:06 +0530

    connect event added ✔

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,4 +1,45 @@
 const os = require('os');
+const io = require('socket.io-client');
+let socket = io('http://127.0.0.1:8181');
+
+socket.on('connect', () => {
+  // console.log('I connected to the socket server... hooray!')
+
+  const nI = os.networkInterfaces();
+  let macA;
+  // loop through all the nI for this machine and find a non-internal one
+  for (let key in nI) {
+    if (!nI[key][0].internal) {
+      if (nI[key][0].mac === '00:00:00:00:00:00') {
+        macA = Math.random().toString(36).substr(2, 15);
+      } else {
+        macA = nI[key][0].mac;
+      }
+      break;
+    }
+  }
+
+  // client auth with single key value
+  socket.emit('clientAuth', '5t78yuhgirekjaht32i3');
+
+  performanceData().then((allPerformanceData) => {
+    allPerformanceData.macA = macA;
+    socket.emit('initPerfData', allPerformanceData);
+  });
+
+  // start sending over data on interval
+  let perfDataInterval = setInterval(() => {
+    performanceData().then((allPerformanceData) => {
+      // console.log(allPerformanceData)
+      allPerformanceData.macA = macA;
+      socket.emit('perfData', allPerformanceData);
+    });
+  }, 1000);
+
+  socket.on('disconnect', () => {
+    clearInterval(perfDataInterval);
+  });
+});
 
 function performanceData() {
   return new Promise(async (resolve, reject) => {
