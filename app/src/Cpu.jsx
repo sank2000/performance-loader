359d69f4d773c3653aff876eeda581ce8226962d
sank2000 <santhoshvelr@yahoo.com>
2021-07-20 20:34:54 +0530

    canvas animation bug fixed 🚀

--- app/src/Cpu.jsx
+++ app/src/Cpu.jsx
@@ -1,8 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import drawCircle from './utilities/canvasLoadAnimation';
 
 function Cpu(props) {
   const canvasRef = useRef();
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      drawCircle(canvasRef.current, props.cpuData.cpuLoad);
+    }
+  }, [props]);
 
   return (
     <div className="col-sm-3 cpu">
--- app/src/Mem.jsx
+++ app/src/Mem.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import drawCircle from './utilities/canvasLoadAnimation';
 
 function Mem(props) {
-  // console.log(props);
-  const { totalMem, usedMem, memUsage, freeMem } = props.memData;
-  // const canvas = document.querySelector(`.${props.memData.memWidgetId}`);
-  // drawCircle(canvas, memUsage * 100);
-  // console.log(memUsage);
-  const totalMemInGB = ((totalMem / 1073741824) * 100) / 100;
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      drawCircle(canvasRef.current, props.memData.memUsage * 100);
+    }
+  }, [props]);
+
+  const { totalMem, memUsage, freeMem } = props.memData;
+  const totalMemInGB = Math.floor((totalMem / 1073741824) * 100) / 100;
   const freeMemInGB = Math.floor((freeMem / 1073741824) * 100) / 100;
   return (
     <div className="col-sm-3 mem">
       <h3>Memory Usage</h3>
       <div className="canvas-wrapper">
         <canvas
+          ref={canvasRef}
           className={props.memData.memWidgetId}
           width="200"
           height="200"
